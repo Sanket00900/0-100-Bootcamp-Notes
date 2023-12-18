52d4aaff68b402470ed828009e5739aab83e46b7
“Sanket <sanketsj00900@gmail.com>
2023-12-19 01:40:03 +0530

    3.2  updated

--- Week3/3.2-Fetch-Authentication-JWT/express-jwt.js
+++ Week3/3.2-Fetch-Authentication-JWT/express-jwt.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
+const jwtPassword = process.env.jwtSecret;
 
 const app = express();
 app.use(express.json());
