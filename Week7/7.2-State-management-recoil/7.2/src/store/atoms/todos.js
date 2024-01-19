76bb2abcd6cb7298c304f149625bed5fc793c28c
Restyled.io <commits@restyled.io>
2024-01-19 04:34:38 +0000

    Restyled by clang-format

--- Week7/7.2-State-management-recoil/7.2/src/store/atoms/todos.js
+++ Week7/7.2-State-management-recoil/7.2/src/store/atoms/todos.js
@@ -1,36 +1,33 @@
-import { atom, selector } from "recoil";
+import {atom, selector} from "recoil";
 
 export const todosAtom = atom({
-  key: "todosAtom",
-  default: [],
+  key : "todosAtom",
+  default : [],
 });
 
 export const titleAtom = atom({
-  key: "titleAtom",
-  default: "",
+  key : "titleAtom",
+  default : "",
 });
 
 export const descriptionAtom = atom({
-  key: "descriptionAtom",
-  default: "",
+  key : "descriptionAtom",
+  default : "",
 });
 
 export const filterAtom = atom({
-  key: "filterAtom",
-  default: "",
+  key : "filterAtom",
+  default : "",
 });
 
 //! Selector to filter todos based on the filter text
 
 export const filterTodosSelector = selector({
-  key: "filteredTodos",
-  get: ({ get }) => {
+  key : "filteredTodos",
+  get : ({get}) => {
     const todos = get(todosAtom);
     const filter = get(filterAtom).toLowerCase();
-    return todos.filter(
-      (x) =>
-        x.title.toLowerCase().includes(filter) ||
-        x.description.toLowerCase().includes(filter)
-    );
+    return todos.filter((x) => x.title.toLowerCase().includes(filter) ||
+                               x.description.toLowerCase().includes(filter));
   },
 });
