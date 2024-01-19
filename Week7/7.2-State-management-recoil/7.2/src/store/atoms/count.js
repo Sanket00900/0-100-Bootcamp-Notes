d5f7e02d92fcdad6c9dadc614d56a73e8889b594
Restyled.io <commits@restyled.io>
2024-01-19 04:34:43 +0000

    Restyled by prettier

--- Week7/7.2-State-management-recoil/7.2/src/store/atoms/count.js
+++ Week7/7.2-State-management-recoil/7.2/src/store/atoms/count.js
@@ -1,13 +1,13 @@
-import {atom, selector} from "recoil";
+import { atom, selector } from "recoil";
 
 export const countAtom = atom({
-  key : "CountAtom",
-  default : 0,
+  key: "CountAtom",
+  default: 0,
 });
 
 export const evenSelector = selector({
-  key : "evenSelector",
-  get : ({get}) => {
+  key: "evenSelector",
+  get: ({ get }) => {
     const count = get(countAtom);
     return count % 2;
   },
